b2095c553c25349460f48015d7ccfc046186d505
maxjnorman <maxjnorman@gmail.com>
2021-02-10 15:00:03 +0000

    first go finally working OK

--- main.R
+++ main.R
@@ -3,10 +3,21 @@ library(tidyverse)
 library(tidymodels)
 library(glmnet)
 library(vip)
+library(parallel)
+
+all_cores <- detectCores(logical = FALSE)
 
 # scaling functions
+standardise_factory <- function(bounds = c(0, 1)) {
+  standardise_instance <- function(val) {
+    val * (max(bounds) - min(bounds)) + min(bounds)
+  }
+  return(standardise_instance)
+}
 scale <- list()
-scale[["penalty"]] <- function(val) {val * (10^-1 - 10^-4) + 10^-4}
+scale[["penalty"]] <- standardise_factory(bounds = c(10^-1, 10^-4))
+scale[["tree_depth"]] <- standardise_factory(bounds = c(1, 16))
+scale[["cost_complexity"]] <- standardise_factory()
 
 # static vector
 holidays <- c("AllSouls", "AshWednesday", "ChristmasEve", "Easter", 
@@ -76,6 +87,45 @@ logit[["plot"]] <- logit[["result"]] %>%
 logit[["plot"]] 
 
 tree <- list()
-tree[["model"]] <- decision_tree(penalty = tune(), mixture = 1) %>% 
-  set_engine("glmnet") %>%
+tree[["model"]] <- decision_tree(
+  cost_complexity = tune(),
+  tree_depth = tune()
+) %>% 
+  set_engine("rpart") %>% 
   set_mode("classification")
+tree[["grid"]] <- tibble(
+  cost_complexity = runif(150) %>% scale$cost_complexity(),
+  tree_depth = runif(150) %>% scale$tree_depth()
+)
+tree[["recipe"]] <- recipe(children ~ ., data = train) %>% 
+  step_date(arrival_date) %>% 
+  step_holiday(arrival_date, holidays = holidays) %>% 
+  step_rm(arrival_date) %>% 
+  step_dummy(all_nominal(), -all_outcomes())
+tree[["workflow"]] <- workflow() %>% 
+  add_model(tree[["model"]]) %>% 
+  add_recipe(tree[["recipe"]])
+tree[["result"]] <- tree[["workflow"]] %>%
+  tune_grid(
+    val,
+    grid = tree[["grid"]],
+    control = control_grid(save_pred = TRUE),
+    metrics = metric_set(roc_auc)
+  )
+tree[["result"]] %>% show_best("roc_auc", 3)
+tree[["plot-tree_depth"]] <- tree[["result"]] %>% 
+  collect_metrics() %>% 
+  ggplot(aes(x = cost_complexity, y = mean)) + 
+  geom_point() + 
+  geom_line() + 
+  ylab("Area under the ROC Curve") +
+  scale_x_log10(labels = scales::label_number())
+tree[["plot-tree_depth"]]
+tree[["plot-cost_complexity"]] <- tree[["result"]] %>% 
+  collect_metrics() %>% 
+  ggplot(aes(x = tree_depth, y = mean)) + 
+  geom_point() + 
+  geom_line() + 
+  ylab("Area under the ROC Curve") +
+  scale_x_log10(labels = scales::label_number())
+tree[["plot-cost_complexity"]]
