85e94a191f5f71f99fab2e26276832c1e3962657
maxjnorman <maxjnorman@gmail.com>
2021-02-12 12:09:36 +0000

    PCA will now tune number of components (i think!)

--- main.R
+++ main.R
@@ -2,22 +2,26 @@
 library(tidyverse)
 library(tidymodels)
 library(glmnet)
-library(vip)
-library(parallel)
+# library(vip)
+# library(parallel)
 
 all_cores <- detectCores(logical = FALSE)
 
 # scaling functions
-standardise_factory <- function(bounds = c(0, 1)) {
+standardise_factory <- function(b = c(0, 1)) {
+  
+  bounds <- b
   standardise_instance <- function(val) {
     val * (max(bounds) - min(bounds)) + min(bounds)
   }
   return(standardise_instance)
 }
+
 scale <- list()
-scale[["penalty"]] <- standardise_factory(bounds = c(10^-1, 10^-4))
-scale[["tree_depth"]] <- standardise_factory(bounds = c(1, 16))
+scale[["penalty"]] <- standardise_factory(b = c(10^-1, 10^-4))
+scale[["tree_depth"]] <- standardise_factory(b = c(1, 16))
 scale[["cost_complexity"]] <- standardise_factory()
+scale[["num_comp"]] <- standardise_factory(c(1, 22))
 
 # static vector
 holidays <- c("AllSouls", "AshWednesday", "ChristmasEve", "Easter", 
@@ -57,14 +61,18 @@ logit <- list() # container for logit model
 logit[["model"]] <- logistic_reg(penalty = tune(), mixture = 1) %>% 
   set_engine("glmnet") %>%
   set_mode("classification")
-logit[["grid"]] <- tibble(penalty = runif(150) %>% scale$penalty())
+logit[["grid"]] <- tibble(
+  penalty = runif(150) %>% scale$penalty(),
+  num_comp = runif(150) %>% scale$num_comp()
+  )
 logit[["recipe"]] <- recipe(children ~ ., data = train) %>% 
   step_date(arrival_date) %>% 
   step_holiday(arrival_date, holidays = holidays) %>% 
   step_rm(arrival_date) %>% 
   step_dummy(all_nominal(), -all_outcomes()) %>% 
-  step_zv(all_predictors()) %>% 
-  step_normalize(all_predictors())
+  step_zv(all_predictors(), -all_outcomes()) %>%
+  step_normalize(all_predictors(), -all_outcomes()) %>%
+  step_pca(-children, num_comp = tune())
 logit[["workflow"]] <- workflow() %>% 
   add_model(logit[["model"]]) %>% 
   add_recipe(logit[["recipe"]])
@@ -72,7 +80,7 @@ logit[["result"]] <- logit[["workflow"]] %>%
   tune_grid(
     val,
     grid = logit[["grid"]],
-    control = control_grid(save_pred = TRUE),
+    control = control_grid(save_pred = TRUE, parallel_over = "everything"),
     metrics = metric_set(roc_auc)
   )
 logit[["result"]] %>% show_best("roc_auc", 3)
@@ -86,46 +94,46 @@ logit[["plot"]] <- logit[["result"]] %>%
   scale_x_log10(labels = scales::label_number())
 logit[["plot"]] 
 
-tree <- list()
-tree[["model"]] <- decision_tree(
-  cost_complexity = tune(),
-  tree_depth = tune()
-) %>% 
-  set_engine("rpart") %>% 
-  set_mode("classification")
-tree[["grid"]] <- tibble(
-  cost_complexity = runif(150) %>% scale$cost_complexity(),
-  tree_depth = runif(150) %>% scale$tree_depth()
-)
-tree[["recipe"]] <- recipe(children ~ ., data = train) %>% 
-  step_date(arrival_date) %>% 
-  step_holiday(arrival_date, holidays = holidays) %>% 
-  step_rm(arrival_date) %>% 
-  step_dummy(all_nominal(), -all_outcomes())
-tree[["workflow"]] <- workflow() %>% 
-  add_model(tree[["model"]]) %>% 
-  add_recipe(tree[["recipe"]])
-tree[["result"]] <- tree[["workflow"]] %>%
-  tune_grid(
-    val,
-    grid = tree[["grid"]],
-    control = control_grid(save_pred = TRUE),
-    metrics = metric_set(roc_auc)
-  )
-tree[["result"]] %>% show_best("roc_auc", 3)
-tree[["plot-tree_depth"]] <- tree[["result"]] %>% 
-  collect_metrics() %>% 
-  ggplot(aes(x = cost_complexity, y = mean)) + 
-  geom_point() + 
-  geom_line() + 
-  ylab("Area under the ROC Curve") +
-  scale_x_log10(labels = scales::label_number())
-tree[["plot-tree_depth"]]
-tree[["plot-cost_complexity"]] <- tree[["result"]] %>% 
-  collect_metrics() %>% 
-  ggplot(aes(x = tree_depth, y = mean)) + 
-  geom_point() + 
-  geom_line() + 
-  ylab("Area under the ROC Curve") +
-  scale_x_log10(labels = scales::label_number())
-tree[["plot-cost_complexity"]]
+# tree <- list()
+# tree[["model"]] <- decision_tree(
+#   cost_complexity = tune(),
+#   tree_depth = tune()
+# ) %>% 
+#   set_engine("rpart") %>% 
+#   set_mode("classification")
+# tree[["grid"]] <- tibble(
+#   cost_complexity = runif(150) %>% scale$cost_complexity(),
+#   tree_depth = runif(150) %>% scale$tree_depth()
+# )
+# tree[["recipe"]] <- recipe(children ~ ., data = train) %>% 
+#   step_date(arrival_date) %>% 
+#   step_holiday(arrival_date, holidays = holidays) %>% 
+#   step_rm(arrival_date) %>% 
+#   step_dummy(all_nominal(), -all_outcomes())
+# tree[["workflow"]] <- workflow() %>% 
+#   add_model(tree[["model"]]) %>% 
+#   add_recipe(tree[["recipe"]])
+# tree[["result"]] <- tree[["workflow"]] %>%
+#   tune_grid(
+#     val,
+#     grid = tree[["grid"]],
+#     control = control_grid(save_pred = TRUE),
+#     metrics = metric_set(roc_auc)
+#   )
+# tree[["result"]] %>% show_best("roc_auc", 3)
+# tree[["plot-tree_depth"]] <- tree[["result"]] %>% 
+#   collect_metrics() %>% 
+#   ggplot(aes(x = cost_complexity, y = mean)) + 
+#   geom_point() + 
+#   geom_line() + 
+#   ylab("Area under the ROC Curve") +
+#   scale_x_log10(labels = scales::label_number())
+# tree[["plot-tree_depth"]]
+# tree[["plot-cost_complexity"]] <- tree[["result"]] %>% 
+#   collect_metrics() %>% 
+#   ggplot(aes(x = tree_depth, y = mean)) + 
+#   geom_point() + 
+#   geom_line() + 
+#   ylab("Area under the ROC Curve") +
+#   scale_x_log10(labels = scales::label_number())
+# tree[["plot-cost_complexity"]]
